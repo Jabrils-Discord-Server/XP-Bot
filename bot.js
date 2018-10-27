9f3dd32aed6f95b0535e1e2961a88b6126107a00
Adam K <43103068+canarado@users.noreply.github.com>
2018-10-26 23:20:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("canarado sleep O_o", {type: "WATCHING"});
 });
 
 client.on('message', msg => {
