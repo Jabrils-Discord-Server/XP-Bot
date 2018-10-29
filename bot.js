58294331bd791c6ee368bcfd59ffee92ea0bdb66
Adam K <43103068+canarado@users.noreply.github.com>
2018-10-29 13:05:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('message', async msg => {
      let embed = new Discord.RichEmbed()
      .setTitle('XP-Bot - Commands:')
      .setColor(embed_color)
-     .addField("Current prefix is ${prefix}")
+     .addField("Current prefix is " + prefix)
      .setDescription('`' + prefix + ' help`  displays this message\n'
                    + '`' + prefix + ' status`  shows your verification status');
       
