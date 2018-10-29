3d7f51bf41f6ccf6fb4dc4a6a9a9ea6000b672f2
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-10-29 14:57:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,22 +8,6 @@ module.exports ={
   embed_color: config.embed_color_default
 }
 
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    client.commands.set(props.help.name, props);
-  });
-});
-
 client.on("ready", async () => {
 
   client.user.setPresence({ game: { name: 'you', type: 'WATCHING' }, status: 'online' })
@@ -33,16 +17,7 @@ client.on("ready", async () => {
 client.on("message", async msg => {
   if(msg.author.bot) return;
   if(msg.channel.type === "dm") return;
-
-
-  let prefix = config.prefix;
-  let messageArray = msg.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  let commandfile = client.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(client,msg,args);
-
+  client.say("huh")
 
 });
 
