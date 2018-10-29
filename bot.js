cdbc48cd2e7ff1ef0c7987db72461a1b239a7f44
Sven Fehler <sven.fehler@web.de>
2018-10-29 14:54:03 +0100

    added command prefix

--- bot.js
+++ bot.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = '!xp';
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity("canarado sleep O_o", {type: "WATCHING"});
 });
 
 client.on('message', async msg => {
-  if (msg.content === '!xp ping') {
+  if (!msg.content.includes(prefix)) return;
+  if (msg.content == 'ping') {
     msg.reply('Pong!');
   }
 });
