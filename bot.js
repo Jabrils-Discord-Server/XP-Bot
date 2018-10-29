515e4b5d85bf9f536872a6b77dfc6aa752e02c8b
Sven Fehler <sven.fehler@web.de>
2018-10-29 15:35:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,16 @@ client.on('ready', () => {
   client.user.setActivity("canarado sleep O_o", {type: "WATCHING"});
 });
 
+function test() {
+  setTimeout(function(){
+    client.user.setActivity("canarado sleep O_o", {type: "WATCHING"});
+    setTimeout(function(){
+      client.user.setActivity("your mom", {type: "WATCHING"});
+      test();
+    }, 3000);
+  }, 3000);
+}
+
 client.on('message', async msg => {
   if (!msg.content.includes(prefix)) return;
   if (msg.content.includes('ping')) {
@@ -17,10 +27,10 @@ client.on('message', async msg => {
   }
   if (msg.content.includes('help') || msg.content.includes('-h') || msg.content.includes('?')) {
      let embed = new Discord.RichEmbed()
-     .setTitle('XP-Bot - Help:')
+     .setTitle('XP-Bot - Commands:')
      .setColor(embed_color)
-     .setDescription('`' + prefix + ' help`:  displays this message\n'
-                   + '`' + prefix + ' status`:  shows your verification status');
+     .setDescription('`' + prefix + ' help`  displays this message\n'
+                   + '`' + prefix + ' status`  shows your verification status');
       
      msg.channel.send(embed);
   }
