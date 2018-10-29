0ab269319331fb68615e6135a4b17373582a6ef6
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-10-29 15:04:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,18 +8,45 @@ module.exports ={
   embed_color: config.embed_color_default
 }
 
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+  });
+});
+
 client.on("ready", async () => {
 
-  client.user.setPresence({ game: { name: 'huh', type: 'WATCHING' }, status: 'online' })
+  client.user.setPresence({ game: { name: 'bullshit', type: 'WATCHING' }, status: 'idle' })
 
 });
 
 client.on("message", async msg => {
   if(msg.author.bot) return;
   if(msg.channel.type === "dm") return;
-  msg.channel.send("huh");
+  
+  msg.channel.send("what a minute");
+  
+  let prefix = config.prefix;
+  let messageArray = msg.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = client.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(client,msg,args);
+
 
 });
+
 
 
 client.login(process.env.TOKEN);
