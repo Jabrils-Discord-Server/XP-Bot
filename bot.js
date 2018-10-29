fac904c6bebf4d8dc058b453ee7be2f8f3cc083a
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-10-29 15:01:56 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const config = require("./config.json")
 const Discord = require("discord.js");
+const fs = require("fs");
 const client = new Discord.Client({disableEveryone: true});
 client.commands = new Discord.Collection();
 
