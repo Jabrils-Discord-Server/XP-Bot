36894e977ac9838f9ef25f3976969c6d71d717a0
Sven Fehler <sven.fehler@web.de>
2018-11-07 22:50:44 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,8 @@ module.exports.run = async (client, msg, args) => {
   let embed = new Discord.RichEmbed()
     .setTitle('XP-Bot - Commands:')
     .setColor(data.embed_color)
-    .setDescription('`help`  displays this message\n'
-                  + '`status`  shows your verification status');
+    .setDescription('`help`\ndisplays this help\n\n\n'
+                  + '`status [WATCHING/PLAYING] [MESSAGE]`\nsets the status message of XP-Bot\n\n\n');
     msg.channel.send(embed);
 }
 
