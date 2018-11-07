a53ecf9bbf692da9f3bee92e888c6fbefc8b1fb3
Sven Fehler <sven.fehler@web.de>
2018-11-07 22:59:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ fs.readdir("./commands/", (err, files) => {
 
 client.on("ready", async () => {
 
-  //client.user.setPresence({ game: { name: 'over you ♥', type: 'WATCHING' }, status: 'online' });
+  client.user.setPresence({status: 'online'});
   client.user.setActivity("your mom", { type: "WATCHING" });
 
 });
