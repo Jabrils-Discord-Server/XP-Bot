779e6b4f2c11fed6fd792485e4409c85b6137733
Sven Fehler <sven.fehler@web.de>
2018-11-08 21:30:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,7 +101,7 @@ catch(err) {
 
 function includes_owo(msgc) {
     try {
-        let owolist = ["owo", "uwu", "ywy", "0w0"];
+        let owolist = ["owo", "uwu", "ywy", "0w0", "3w3"];
         for(let i = 0; i < owolist.length; i++) if(msgc.includes("owo") || msgc.includes("uwu")) return true;
         else return false;
     }
