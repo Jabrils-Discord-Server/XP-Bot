cd7a20a1f939659549e22a111e0aae5c75dec834
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-27 17:01:00 -0500

    Create status.js

---
+++ commands/status.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, [args, ...activity]) => {
+    try {
+    let actType = args[0].toLowerCase()
+
+    client.user.setActivity(activity, { type: `${actType}`})
+
+    message.reply(`Set activity to: **${activity}**`)
+    } catch (err) {
+        console.log(err)
+    }
+}
