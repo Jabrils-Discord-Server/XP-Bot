a51d849d58192941f13285e3b21539140ca159a7
Sven Fehler <sven.fehler@web.de>
2018-12-01 00:49:25 +0100

    Update team.js

--- commands/team.js
+++ commands/team.js
@@ -1,17 +1,19 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const http = require('http');
 
 exports.run = (client, message, args) => {
-    let logChannel = '489605729624522762'
+    let logChannel = '489605729624522762';
 
-    let teamMates = message.content.toString().split(" ")
-    teamMates = teamMates[teamMates.length - 1]
-    console.log(teamMates)
+    let teamMates = message.content.toString().split(" ");
+    teamMates = message.author.id + " and " + teamMates[teamMates.length - 1];
+    console.log(teamMates);
+    
+    http.request("POST", "http://sv443.ddns.net/mphost", "xmas_jam_team_submission:" + teamMates);
 
     let embed = new Discord.RichEmbed()
     .setTitle('New team submitted:')
     .setColor(client.config.embed_color_default)
-    .setDescription(`**${teamMates}**\n just formed a team!`)
-    .addField("Team submitted by: ", `${message.author.nickname}`)
+    .setDescription(`**${teamMates}**\n just formed a team!`);
 
-    client.channels.get(logChannel).send(embed)
+    client.channels.get(logChannel).send(embed);
 }
