1e18538588c1d0659e49d8ca149bd95df482212c
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-30 16:42:27 -0500

    Update team.js

--- commands/team.js
+++ commands/team.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js')
 
+exports.run = (client, message, args) => {
+    let logChannel = '489605729624522762'
+
+    let teamMates = args.join(': ')
+
+    let embed = new Discord.RichEmbed()
+    .setTitle('New team submitted:')
+    .setColor(client.config.embed_color_default)
+    .setDescription(`**${teamMates}**\n just formed a team!`)
+
+    client.channels.get(logChannel).send(embed)
+}
