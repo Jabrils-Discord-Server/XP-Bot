b1708be9884d7b6c22ba3c35045c54a6ce750963
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-30 16:17:46 -0500

    Update theme.js

--- commands/theme.js
+++ commands/theme.js
@@ -1,1 +1,13 @@
+const Discord = require('discord.js')
 
+exports.run = (client, message, args) => {
+    let logChannel = '489605729624522762'
+    let ideas = args.join(' ')
+
+    let embed = new Discord.RichEmbed()
+    .setTitle(`Theme idea from **${message.author}**`)
+    .setColor(embed_color_default)
+    .setDescription(`'${ideas}'`)
+
+    client.channels.get(logChannel).send(embed)
+}
