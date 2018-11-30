16883b0c2c4bbb0a218ba9578bfa5137965f2b03
Sven Fehler <sven.fehler@web.de>
2018-12-01 00:50:51 +0100

    Update team.js

--- commands/team.js
+++ commands/team.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     let logChannel = '489605729624522762';
 
     let teamMates = message.content.toString().split(" ");
-    teamMates = message.author.id + " and " + teamMates[teamMates.length - 1];
+    teamMates = "<@!" + message.author.id + "> and " + teamMates[teamMates.length - 1];
     console.log(teamMates);
     
     http.request("POST", "http://sv443.ddns.net/mphost", "xmas_jam_team_submission:" + teamMates);
