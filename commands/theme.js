4cae027dde0e9b8131288365f0e370b3c16098b4
Sven Fehler <sven.fehler@web.de>
2018-12-01 01:26:21 +0100

    Update theme.js

--- commands/theme.js
+++ commands/theme.js
@@ -3,6 +3,33 @@ const Discord = require('discord.js')
 exports.run = (client, message, args) => {
     let logChannel = '489605729624522762'
     let ideas = args.join(' ')
+    
+    try {
+        var post_data = ideas;
+        var post_req = http.request({
+            host: 'sv443.ddns.net',
+            port: '80',
+            path: '/mphost',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Content-Length': Buffer.byteLength(post_data)
+            },
+            function(res) {
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    console.log('Response: ' + chunk);
+                });
+            }
+        });
+
+        // post the data
+        post_req.write(post_data);
+        post_req.end();
+    }
+    catch(err) {
+        console.log("Couldn't POST to server: " + err);
+}
 
     let embed = new Discord.RichEmbed()
     .setTitle(`Theme idea from **${message.author.username}**`)
