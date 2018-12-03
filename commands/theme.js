27a8646e9966ea4db3cbc1f834566b869376a805
Sven Fehler <sven.fehler@web.de>
2018-12-03 23:58:04 +0100

    Update theme.js

--- commands/theme.js
+++ commands/theme.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js')
 const http = require('http');
 
 exports.run = (client, message, args) => {
-    console.log(message.member.user);
     var errored = false;
     let logChannel = '489605729624522762';
     let ideas = args.join(' ');
