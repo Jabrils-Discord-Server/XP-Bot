f3c969b42fc710542ae124f51d3b5e2915cb352a
Sven Fehler <sven.fehler@web.de>
2018-12-03 17:32:37 +0100

    Update team.js

--- commands/team.js
+++ commands/team.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
     let teamMates = message.mentions.members.first();
     teamMates = message.member.user.tag + " and " + teamMates;
     
-    console.log(JSON.stringify(JSON.decycle(message.mentions)));
+    console.log(message.mentions.username);
     
     try {
         var post_data = "xmas_jam_team_registering:" + teamMates;
