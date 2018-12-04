9de36d8bdea7707384260c2630f7c3c9b8be96d8
Sven Fehler <sven.fehler@web.de>
2018-12-04 13:48:42 +0100

    Update gamejam.js

--- commands/gamejam.js
+++ commands/gamejam.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
 let embed = new Discord.RichEmbed()
                     .setTitle('Our next Game Jam:')
                     .setColor(client.config.embed_color_default)
-                    .setDescription('Our next Game Jam will be the XMAS Jam\n\nDuration: 7 days\n\n\nBegins on: December 15th\n    Ends on: December 22nd\n    Voting Ends on: TBA');
+                    .setDescription('Our next Game Jam will be the XMAS Jam\n\nDuration: 7 days\n\n\nBegins on: December 15th\n    Ends on: December 22nd\n    Voting Ends on: TBA\n\n\n**Commands (please use them in #bot-playground):**\n\nTo submit a theme, please use\n`xp? theme *your submission*`\n\nTo form a team, please use\n`xp? team *@-pings of your teammates (don\'t include your own name)*`');
                     message.channel.send(embed)
                     message.delete()
 }
