b5c9ece33fb7b6b275ff8b0bf6d89f3573f538ea
Sven Fehler <sven.fehler@web.de>
2018-12-09 22:01:33 +0100

    Create testhttp.js

---
+++ commands/testhttp.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const http = require('http');
+
+exports.run = (client, message, args) => {
+    var invalid = false;
+    let logChannel = '489605729624522762';
+    let input = args.join(' ');
+    
+}
