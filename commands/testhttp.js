2d510331533c59410c3477173c164f0535b89431
Sven Fehler <sven.fehler@web.de>
2018-12-09 22:11:13 +0100

    Update testhttp.js

--- commands/testhttp.js
+++ commands/testhttp.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const http = require('http');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 exports.run = (client, message, args) => {
     var invalid = false;
@@ -7,4 +8,15 @@ exports.run = (client, message, args) => {
     let input = args.join(' ');
     
     message.reply("Got input \"" + input + "\"");
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://sv443.ddns.net/mphost", true);
+    xhr.addEventListener('load', function(e) {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            message.reply("success");
+        } else {
+            message.reply("error");
+        }
+    });
+	xhr.send("nbr:" + input);
 }
