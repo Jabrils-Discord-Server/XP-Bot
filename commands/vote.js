fd20994f4ab204b290e63194dea46f7beefb7c2b
Sven Fehler <sven.fehler@web.de>
2018-12-09 21:24:30 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -4,7 +4,7 @@ const http = require('http');
 exports.run = (client, message, args) => {
     var invalid = false;
     let logChannel = '489605729624522762';
-    let vote = args.join(' ');
+    let input = args.join(' ');
 	var themes = [
 		"Test",
 		"I am a theme",
@@ -13,8 +13,8 @@ exports.run = (client, message, args) => {
 	var votedfor = 0;
 	
 	try {
-		vote = parseInt(vote);
-		if(vote > themes.length) {
+		votedfor = parseInt(input);
+		if(votedfor > themes.length) {
 			invalid = true;
 			message.reply("please only use a number from 1 to" + themes.length);
 		}
