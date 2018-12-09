92b64e8665943944770f13bb51feda298595ad00
Sven Fehler <sven.fehler@web.de>
2018-12-09 21:38:12 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -13,15 +13,17 @@ exports.run = (client, message, args) => {
 	var votedfor = 0;
 	
 	if(input == "" || input == null || input == undefined) {
+		invalid = true;
+		
 		var content = "EMPTY";
 		for(let i = 0; i < themes.length; i++) content += (i + ": " + themes[i] + "\n");
 		
 		let embed = new Discord.RichEmbed()
-        .setTitle(`Themes - vote with xp? vote \*number\*`)
-        .setColor(client.config.embed_color_default)
-        .setDescription(content);
+        	.setTitle(`Themes - vote with xp? vote \*number\*`)
+        	.setColor(client.config.embed_color_default)
+        	.setDescription(content);
 
-        message.channel.send(embed);
+        	message.channel.send(embed);
 		
 		message.delete();
 	}
@@ -43,11 +45,11 @@ exports.run = (client, message, args) => {
 		message.reply("you have successfully voted for theme " + votedfor);
 		
 		let embed = new Discord.RichEmbed()
-        .setTitle(`Vote from **${message.author.username}**`)
-        .setColor(client.config.embed_color_default)
-        .setDescription(`'${votedfor}'`);
+        	.setTitle(`Vote from **${message.author.username}**`)
+        	.setColor(client.config.embed_color_default)
+        	.setDescription(`'${votedfor}'`);
 
-        client.channels.get(logChannel).send(embed);
+        	client.channels.get(logChannel).send(embed);
 		
 		message.delete();
 	}
