cd9915189fcc685f686b00194d0af483ac0ccdc1
Sven Fehler <sven.fehler@web.de>
2018-12-17 11:47:33 +0100

    Update gamejam.js

--- commands/gamejam.js
+++ commands/gamejam.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
 let embed = new Discord.RichEmbed()
                     .setTitle('Our next Game Jam:')
                     .setColor(client.config.embed_color_default)
-                    .setDescription('Our next Game Jam will be the XMAS Jam\n\nDuration: 7 days\n\n\nBegins on: December 15th\n    Ends on: December 22nd\n    Voting Ends on: December 29th\n\n\n**Commands (please use them in #bot-playground):**\n\nTo submit a theme, please use\n`xp? theme *your submission*`\n\nTo form a team, please use\n`xp? team *@-pings of your teammates (don\'t include your own name)*`');
+                    .setDescription('The XMAS Jam is currently running!\nVisit this page for all information: https://itch.io/jam/cult-of-jabrils-xmas-jam\n\nDuration: 7 days\n\n\nBegins on: December 15th\n    Ends on: December 22nd\n    Voting Ends on: December 29th\n\n\n**Commands (not available during a Game Jam - please use them in #bot-playground):**\n\nTo submit a theme, please use\n`xp? theme *your submission*`\n\nTo form a team, please use\n`xp? team *@-pings of your teammates (don\'t include your own name)*`');
                     message.channel.send(embed)
                     message.delete()
 }
