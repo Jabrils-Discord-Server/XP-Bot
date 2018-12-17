46c21a93dbb4a36e399d6980d2b61d5290896653
Sven Fehler <sven.fehler@web.de>
2018-12-17 12:10:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ const client = new Discord.Client();
 const config = require("./config.json");
 client.config = config;
 
-const logChannel = "489605729624522762";
-
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
@@ -31,11 +29,3 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 client.login(process.env.TOKEN);
-
-client.on("ready", ()=>{
-    let embed = new Discord.RichEmbed()
-        .setTitle('I just redeployed!')
-        .setColor(client.config.embed_color_default)
-        .setDescription(new Date());
-    client.channels.get(logChannel).send(embed);
-});
