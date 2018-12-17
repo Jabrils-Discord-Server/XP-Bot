5fa904907a9fb08f6468283469011c62c8964fa4
Sven Fehler <sven.fehler@web.de>
2018-12-17 11:58:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
-const fs = require("fs"); 
+const fs = require("fs");  
 
 const client = new Discord.Client();
 const config = require("./config.json");
