a0b085e6d4492a9d6e37db47e6d303df27bd7dd3
Sven Fehler <sven.fehler@web.de>
2018-12-17 12:12:00 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,13 @@
+const logChannel = "489605729624522762";
+
 module.exports = (client) => {
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 
-    client.user.setActivity(`Sv sleep O.o`, { type: 'WATCHING' });
+    let embed = new Discord.RichEmbed()
+        .setTitle('I just redeployed!')
+        .setColor(client.config.embed_color_default)
+        .setDescription(`${new Date()}\nServing in ${client.channels.size} channels on ${client.guilds.size} servers for a total of ${client.users.size} users.`);
+    client.channels.get(logChannel).send(embed);
+    
+    client.user.setActivity(`y'all fail at the Jam`, { type: 'WATCHING' });
 }
