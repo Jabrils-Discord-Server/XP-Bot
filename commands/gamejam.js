416b4d672377f8c279db9ee25fada08261bde767
Sven Fehler <sven.fehler@web.de>
2018-12-23 13:36:51 +0100

    Update gamejam.js

--- commands/gamejam.js
+++ commands/gamejam.js
@@ -11,7 +11,7 @@ let embed = new Discord.RichEmbed()
                                     + 'Began on: December 15th\n'
                                     + 'Ended on: December 22nd\n'
                                     + 'Voting Ended on: December 29th\n\n\n\n\n'
-                                    + 'Next Game Jam: TBA (but probably in February or March)');
+                                    + 'Next Game Jam: TBA (but probably in February or March)'
                                     /*+ '**Commands (not available during a Game Jam - please use them in #bot-playground):**\n\n'
                                     + 'To submit a theme, please use\n`xp? theme *your submission*`\n\n'
                                     + 'To form a team, please use\n`xp? team *@-pings of your teammates (don\'t include your own name)*`'*/);
