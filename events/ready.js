36d194fc5f197edfd09dcbffc4e1e1312f085e45
Sven Fehler <sven.fehler@web.de>
2018-12-29 16:20:16 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 
     let embed = new Discord.RichEmbed()
-        .setTitle('I just redeployed!')
+        .setTitle('I am better than Yabe!')
         .setColor(client.config.embed_color_default)
         .setTimestamp()
         .setDescription(`Serving in ${client.channels.size} channels on ${client.guilds.size} servers for a total of ${client.users.size} users.`);
