6ffc888f8b01efb886101f4d28b2923dcde7ea43
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-30 13:39:12 -0500

    maybe owo fix

--- events/message.js
+++ events/message.js
@@ -3,20 +3,22 @@ module.exports = (client, message) => {
   
     if (message.content.indexOf(client.config.prefix) !== 0) return;
   
-    let owolist = ["owo", "uwu", "ywy", "0w0", "3w3", "øwø", "ôwô", "õwõ", "OuO", "OwO", "UwU"]
-    owolist.forEach(x => {
-        if(message.content.includes(x)) {
-            message.delete()
-        }
-    })
+    
  
     
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-  
+    
     const cmd = client.commands.get(command);
   
     if (!cmd) return;
 
     cmd.run(client, message, args);
+    
+    let owolist = ["owo", "uwu", "ywy", "0w0", "3w3", "øwø", "ôwô", "õwõ", "OuO", "OwO", "UwU"]
+    owolist.forEach(x => {
+           if(message.content.includes(x)) {
+               message.delete()
+            }
+    })
   };
