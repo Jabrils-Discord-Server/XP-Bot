ee749419fe176a54b9c57384607a621837c84f6c
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-30 13:43:42 -0500

    owo fix pls

--- events/message.js
+++ events/message.js
@@ -2,18 +2,9 @@ module.exports = (client, message) => {
     if (message.author.bot) return;
   
     if (message.content.indexOf(client.config.prefix) !== 0) return;
-  
-    
- 
     
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    
-    const cmd = client.commands.get(command);
-  
-    if (!cmd) return;
-
-    cmd.run(client, message, args);
     
     let owolist = ["owo", "uwu", "ywy", "0w0", "3w3", "øwø", "ôwô", "õwõ", "OuO", "OwO", "UwU"]
     owolist.forEach(x => {
@@ -22,4 +13,10 @@ module.exports = (client, message) => {
                return message.delete()
             }
     })
+    
+    const cmd = client.commands.get(command);
+  
+    if (!cmd) return;
+
+    cmd.run(client, message, args);
   };
