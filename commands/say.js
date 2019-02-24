407f25c0cfabf1e6cc8b4a55247bbfe5fde8e60c
Sven Fehler <sven.fehler@web.de>
2019-02-24 13:11:31 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,8 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
     if(message.member.permissions.has("MANAGE_MESSAGES")) {
         message.delete().then(m=>{
-            message.reply(args);
-            return message.channel.send(args);
+            return message.channel.send(args.join(" "));
         });
     }
     else return message.reply("you don't have enough permissions to do that!");
