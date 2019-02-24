9e81f66d2b619be19f9ca46bbe065e45f349a6e0
Sven Fehler <sven.fehler@web.de>
2019-02-24 13:06:06 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,34 +1,10 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    message.reply(message.member.permissions.has("MANAGE_MESSAGES") + " - " + message.content + " - " + args);
-    /*var sayInChannel, sendMsg;
-
-    if(args == null) return message.delete();
-    if(args.split(" ")[0].split("")[0] == "<" && args.split(" ")[0].split("")[1] == "#") {
-        sayInChannel = args.split(" ")[0];
-        sayInChannel = sayInChannel.substring(2, sayInChannel.length - 1);
-        sendMsg = args.split(" ");
-        sendMsg.shift();
-        sendMsg = sendMsg.join(" ");
-    }
-
     if(message.member.permissions.has("MANAGE_MESSAGES")) {
-        if(sayInChannel == null) {
-            message.delete().then(m=>{
-                message.channel.send(args);
-            });
-        }
-        else {
-            try {
-                message.guild.channels.get(sayInChannel).send(sendMsg);
-                message.react("✅");
-                message.delete(5000);
-            }
-            catch(err) {
-                message.channel.send(args);
-            }
-        }
+        message.delete().then(m=>{
+            return message.channel.send(args);
+        });
     }
-    else return message.reply("you don't have enough permissions to do that!");*/
+    else return message.reply("you don't have enough permissions to do that!");
 }
