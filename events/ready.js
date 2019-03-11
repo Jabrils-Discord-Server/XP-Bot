b4ea93aa262629f0fc3c5b5be228c04e72461fb7
Adam K <43103068+canarado@users.noreply.github.com>
2019-03-11 13:20:54 -0400

    :)

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,17 @@ module.exports = (client) => {
         .setTimestamp()
         .setDescription(`Serving in ${client.channels.size} channels on ${client.guilds.size} servers for a total of ${client.users.size} users.`);
     client.channels.get(logChannel).send(embed)
-        
+       
+    let actarr = ['Noburuu shower', 'Gabe suck a fat dicc', 'Sv be super gay', 'canny is the best uwu', 'fuck all mods but ceni',' canarado best arbiter']
     
-    client.user.setActivity(`Noboruu shower`, { type: 'WATCHING' });
+    client.user.setActivity(`${actarr.random()}`, {type: 'WATCHING'})
+    
+    setInterval(() => {
+        client.user.setActivity(`${actarr.random}`, {type: 'WATCHING'})
+    }, 1000 * 60 * 3)
+    // client.user.setActivity(`Noboruu shower`, { type: 'WATCHING' });
+}
+
+Array.prototype.random() {
+ return this[Math.floor((Math.random()*this.length))];   
 }
