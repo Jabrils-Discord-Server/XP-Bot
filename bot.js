4c44c8c12a92abee5bd35cc405e33539317f7f59
Sven Fehler <sven.fehler@web.de>
2019-03-17 22:36:53 +0100

    just redeploying it bih

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");  
-
+ 
 const client = new Discord.Client();
 const config = require("./config.json");
 client.config = config;
