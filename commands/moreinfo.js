53110796406b56b6ef9188ab74f311ca03640d32
Marcel Hauser <maha93@gmx.de>
2019-06-19 15:06:50 +0200

    Update moreinfo.js
    optimized the code & changed it to the same style as #9

--- commands/moreinfo.js
+++ commands/moreinfo.js
@@ -2,19 +2,16 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
 
-    let resp1 = ["I get Syntax Error please help",
-                 "I need help with code it doesn't work"];
+    responses = [
+        {wrong: "I get Syntax Error please help", right: "I got the following error in C#, can someone please help?\nSyntax Error, ':' expected\n\nThis is my code:\n(code here)"},
+        {wrong: "I need help with code it doesn't work", right: "I have the following Python code but when I run it it doesn't work nor does it give an error:\n(code here)"}
+    ];
     
-    let resp2 = ["I got the following error in C#, can someone please help?\nSyntax Error, ':' expected\n\nThis is my code:\n(code here)",
-                 "I have the following Python code but when I run it it doesn't work nor does it give an error:\n(code here)"];
-    
-    let randIndex = Math.floor(Math.random()*resp1.length);
-    
-    let chosenResponse = "We want to help you but to do that we need more information.\n For example, instead of asking: \n```\n" + resp1[randIndex] + "\n```\nInstead ask: \n```\n" + resp2[randIndex] + "\n```";    
+    chosenResponse = responses[Math.floor(Math.random()*responses.length)];
     
     let embed = new Discord.RichEmbed()
     .setTitle("Please provide more information!")
-    .setDescription(chosenResponse)
+    .setDescription("We want to help you but to do that we need more information.\n For example, instead of asking: \n```\n${chosenResponse.wrong}\n```\nInstead ask: \n```\n${chosenResponse.right}\n```")
     .addBlankField()
     .addField("Please make sure your question contains the following:", "- A code snippet\n\n- The programming language of that snippet\n\n- A good and in-depth explanation what you want to achieve with that code and what went wrong")
     .addBlankField()
