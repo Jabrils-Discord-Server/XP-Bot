3fa65e95c7a460c9406ce74df0c571a15f6d5321
Sven Fehler <sven.fehler@web.de>
2020-03-19 15:09:22 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 module.exports = (client, message) => {
-    /*
+    
     try {
-        if(message.content.toString().replace(/\s*/gm, "").toLowerCase().includes("xd") && message.author.id != '427491040468140043') message.react("493187615659393025");//return message.delete();
-        if(message.content.toString().toLowerCase().includes("squint") || message.content.toString().toLowerCase().includes("cough") && message.author.id != '427491040468140043') message.react("492785060869832706");
+        // if(message.content.toString().replace(/\s*/gm, "").toLowerCase().includes("xd") && message.author.id != '427491040468140043') message.react("493187615659393025");//return message.delete();
+        // if(message.content.toString().toLowerCase().includes("squint") || message.content.toString().toLowerCase().includes("cough") && message.author.id != '427491040468140043') message.react("492785060869832706");
         if(message.content.toString().toLowerCase().includes("indoo") || message.content.toString().toLowerCase().includes("indede") && message.author.id != '427491040468140043') message.react("522675592747810826");
-        if(message.content.toString().toLowerCase().includes("lurk") && message.author.id != '427491040468140043') message.react("500645964835717120");
-        if(message.content.toString().toLowerCase().includes("bruh moment") && message.author.id != "427491040468140043") message.reply("https://youtu.be/V263ad2e2uk");
+        // if(message.content.toString().toLowerCase().includes("lurk") && message.author.id != '427491040468140043') message.react("500645964835717120");
+        // if(message.content.toString().toLowerCase().includes("bruh moment") && message.author.id != "427491040468140043") message.reply("https://youtu.be/V263ad2e2uk");
     }
     catch(err) {
         console.log(`Error: ${err}`);   
     }
-    */
+    
     
     if (message.author.bot) return;
     if(message.content.toString().toLowerCase().match(/(\sarch[^\w])|(\sarch$)|(^arch$)|(^arch\s)/gm)) {
